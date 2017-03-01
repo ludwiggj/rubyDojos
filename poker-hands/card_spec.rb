ce66d5ae5e49a5cb355f1e65e95505ffe3ee3ded
Graeme Ludwig <graeme.ludwig@btopenworld.com>
2017-03-01 23:31:46 +0000

    Reorganised tests

--- poker-hands/card_spec.rb
+++ poker-hands/card_spec.rb
@@ -8,37 +8,28 @@ describe Card do
     expect(Card.new("10D").to_s).to eq("10D")
   end
 
-  it "< operator can compare cards" do
-    expect(Card.new("2H") < Card.new("3H")).to eq(true)
-    expect(Card.new("2H") < Card.new("JH")).to eq(true)
-    expect(Card.new("QH") < Card.new("KH")).to eq(true)
-    expect(Card.new("10H") < Card.new("AD")).to eq(true)
-    expect(Card.new("10H") <=> Card.new("AD")).to eq(-1)
+  describe "< operator" do
+    it "can compare cards" do
+      expect(Card.new("2H")  < Card.new("3H")).to eq(true)
+      expect(Card.new("2H")  < Card.new("JH")).to eq(true)
+      expect(Card.new("QH")  < Card.new("KH")).to eq(true)
+      expect(Card.new("10H") < Card.new("AD")).to eq(true)
+     end
   end
 
-  it "<=> operator can equate cards" do
-    expect(Card.new("2H")).to eq(Card.new("2C"))
-    expect(Card.new("KH")).to eq(Card.new("KH"))
-    expect(Card.new("2H") <=> Card.new("2C")).to eq(0)
-    expect(Card.new("KH") <=> Card.new("KH")).to eq(0)
+  describe "== operator" do
+    it "can equate cards" do
+      expect(Card.new("2H") == Card.new("2C")).to eq(true)
+      expect(Card.new("KH") == Card.new("KH")).to eq(true)
+      expect(Card.new("KH") == Card.new("QH")).to eq(false)
+    end
+  end
+
+  describe "<=> operator" do
+    it "can compare cards" do
+      expect(Card.new("2H")  <=> Card.new("2C")).to eq(0)
+      expect(Card.new("KH")  <=> Card.new("KH")).to eq(0)
+      expect(Card.new("10H") <=> Card.new("AD")).to eq(-1)
+    end
   end
 end
-
-=begin
-
-describe class
-  # can use subject.
-
-  before # executed before each block that follows
-
-  describe "method"
-    it ""
-
-    context "...."
-    before # executed before each block that follows (its etc.)
-    it ""
-    it ""
-    context ""
-  end
-end    
-=end
